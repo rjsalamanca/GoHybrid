a7a9efd7c4c644d1288538e577a237870b6feab0
RJ Salamanca <salamancaronito@gmail.com>
2019-07-13 19:30:57 -0400

    added bootstrap components

--- gohybrid-react/src/components/usersPage.js
+++ gohybrid-react/src/components/usersPage.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
 
 class UsersPage extends Component {
 
     login() {
         return (
-            <div>
-                <h1>login</h1>
-            </div>
+            <Card>
+                <Card.Header as="h5">Featured</Card.Header>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            // <div>
+            //     <h1>login</h1>
+            // </div>
         )
     }
 
