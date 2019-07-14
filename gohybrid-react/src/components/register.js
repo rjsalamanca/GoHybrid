b3aff3d97cadc89a9d40628a983878e8db5f8e2f
RJ Salamanca <salamancaronito@gmail.com>
2019-07-14 00:21:16 -0400

    created a stateful register component

--- gohybrid-react/src/components/register.js
+++ gohybrid-react/src/components/register.js
@@ -2,40 +2,49 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Button, Form } from 'react-bootstrap';
 
-const loginCard = {
-    width: '500px',
-    margin: '30px auto'
-}
-
-class Login extends Component {
+class Register extends Component {
     state = {
+        first_name: "",
+        last_name: "",
         email: "",
         password: ""
     }
 
+    handleFirstName = (e) => { this.setState({ first_name: e.target.value }) }
+    handleLastName = (e) => { this.setState({ last_name: e.target.value }) }
     handleEmail = (e) => { this.setState({ email: e.target.value }) }
     handlePassword = (e) => { this.setState({ password: e.target.value }) }
 
     render() {
         return (
-            <Card variant={'dark'} style={loginCard}>
-                <Card.Header as="h5">Login</Card.Header>
+            <Card className="usersCard" variant={"dark"}>
+                <Card.Header as="h5">Register</Card.Header>
                 <Card.Body>
                     <Form>
                         <Form.Group controlId="formBasicEmail">
+                            <Form.Label>First Name: </Form.Label>
+                            <Form.Control type="input" onChange={(e) => this.handleFirstName(e)} placeholder="Enter Your First Name" />
+
+                            <Form.Label>Last Name: </Form.Label>
+                            <Form.Control type="input" onChange={(e) => this.handleLastName(e)} placeholder="Enter Your Last Name" />
+
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" onChange={(e) => this.handleEmail(e)} placeholder="Enter email" />
+
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
                         </Form.Group>
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
                             <Form.Control autoComplete="on" type="password" onChange={(e) => this.handlePassword(e)} placeholder="Password" />
                         </Form.Group>
                         <Button variant="primary">
-                            Sign In
+                            Submit
                         </Button>
                     </Form>
                     <p className="mt-4">
-                        No Account? <Link to="/users/register">Register</Link>
+                        Already have an account? <Link to="/users/login">Login Here</Link>
                     </p>
                 </Card.Body>
             </Card>
@@ -43,4 +52,4 @@ class Login extends Component {
     }
 }
 
-export default Login;
+export default Register;
--- gohybrid-react/src/components/usersPage.js
+++ gohybrid-react/src/components/usersPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Login from './login';
+import Register from './register';
 
 class UsersPage extends Component {
 
@@ -14,7 +15,7 @@ class UsersPage extends Component {
     render() {
         const { login_or_register } = this.props.match.params;
         return (
-            (login_or_register === 'login') ? <Login /> : (login_or_register === 'register') ? this.register() : <div>Unknown</div>
+            (login_or_register === 'login') ? <Login /> : (login_or_register === 'register') ? <Register /> : <div>Unknown</div>
         )
     }
 }
