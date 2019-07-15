d0a4394fcb3793a9823e8db25c0a896834acbe4a
RJ Salamanca <salamancaronito@gmail.com>
2019-07-15 17:25:41 -0400

    finished select option drop down

--- gohybrid-react/src/components/homePage.js
+++ gohybrid-react/src/components/homePage.js
@@ -12,7 +12,7 @@ class HomePage extends Component {
         getYears: [],
         getMakes: [],
         getModels: [],
-        year: 2019,
+        year: null,
         make: '',
         model: '',
         searchParameters: ''
@@ -22,11 +22,13 @@ class HomePage extends Component {
         this.loadYears();
     }
 
-    componentDidUpdate = () => {
-        //this.loadMakes();
-
-        console.log('hi')
-    }
+    // componentDidUpdate = () => {
+    //     const { year, make } = this.state;
+    //     if (year !== null) {
+    //         this.loadMakes();
+    //     }
+    //     //this.loadMakes();
+    // }
 
     loadYears = async () => {
         const url = "https://www.fueleconomy.gov/ws/rest/vehicle/menu/year";
@@ -36,7 +38,6 @@ class HomePage extends Component {
             const responseToText = await response.text();
             const dataAsJson = JSON.parse(convert.xml2json(responseToText, { compact: true, spaces: 4 })).menuItems.menuItem;
             const years = dataAsJson.map(year => parseInt(year.value._text));
-
             this.setState({ getYears: years })
         } catch (err) {
             console.log(err.message)
@@ -44,43 +45,48 @@ class HomePage extends Component {
     }
 
     loadMakes = async () => {
-
-        const url = "https://www.fueleconomy.gov/ws/rest/ympg/shared/vehicles?make=Toyota";
-
+        const url = `https://www.fueleconomy.gov/ws/rest/vehicle/menu/make?year=${this.state.year}`;
         try {
             const response = await fetch(url)
             const responseToText = await response.text();
-            const dataAsJson = JSON.parse(convert.xml2json(responseToText, { compact: true, spaces: 4 })).vehicles;
-            // this.setState({ makes: data.Results });
-
-            //var result1 = convert.xml2json(xml, {compact: true, spaces: 4});
-
+            const dataAsJson = JSON.parse(convert.xml2json(responseToText, { compact: true, spaces: 4 })).menuItems.menuItem;
+            const carMakes = dataAsJson.map(make => make.value._text);
+            this.setState({ getMakes: carMakes })
         } catch (err) {
-            // console.log(err.message)
+            console.log(err.message)
         }
     }
 
-    handleMake = (e) => {
+    loadModels = async () => {
+        const url = `https://www.fueleconomy.gov/ws/rest/vehicle/menu/model?year=${this.state.year}&make=${this.state.make}`;
+        try {
+            const response = await fetch(url)
+            const responseToText = await response.text();
+            const dataAsJson = JSON.parse(convert.xml2json(responseToText, { compact: true, spaces: 4 })).menuItems.menuItem;
+            const carModels = dataAsJson.map(model => model.value._text);
+            this.setState({ getModels: carModels })
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
+    handleYear = async (e) => {
+        if (e.target.value !== 'Year') {
+            await this.setState({ year: e.target.value });
+            this.loadMakes();
+        }
+    }
+
+    handleMake = async (e) => {
         if (e.target.value !== 'Make') {
-            this.setState({
-                make: e.target.value
-            })
+            await this.setState({ make: e.target.value })
+            this.loadModels();
         }
     }
 
     handleModel = (e) => {
         if (e.target.value !== 'Model') {
-            this.setState({
-                model: e.target.value
-            })
-        }
-    }
-
-    handleYear = (e) => {
-        if (e.target.value !== 'Year') {
-            this.setState({
-                year: e.target.value
-            })
+            this.setState({ model: e.target.value })
         }
     }
 
@@ -95,7 +101,7 @@ class HomePage extends Component {
                         <Form.Control onChange={(e) => this.handleYear(e)} as="select">
                             <option>Year</option>
                             {getYears.length !== 0 ?
-                                getYears.map((singleYear, index) =>
+                                getYears.map(singleYear =>
                                     <option key={`year-lookup-${singleYear}`}>{singleYear}</option>
                                 )
                                 :
@@ -104,13 +110,23 @@ class HomePage extends Component {
                         </Form.Control>
                         <Form.Control onChange={(e) => this.handleMake(e)} as="select">
                             <option>Make</option>
-                            <option>Toyota</option>
+                            {getMakes.length !== 0 ?
+                                getMakes.map(singleMake =>
+                                    <option key={`manufacturer-${singleMake}`}>{singleMake}</option>
+                                )
+                                :
+                                <option disabled>Choose a Year First...</option>
+                            }
                         </Form.Control>
                         <Form.Control onChange={(e) => this.handleModel(e)} as="select">
                             <option>Model</option>
-                            <option>Rav 4</option>
-                            <option>Camry</option>
-                            <option>Corolla</option>
+                            {getModels.length !== 0 ?
+                                getModels.map(singleModel =>
+                                    <option key={`${make}-${singleModel}`}>{singleModel}</option>
+                                )
+                                :
+                                <option disabled>Choose a Year First...</option>
+                            }
                         </Form.Control>
                     </div>
                     <div id="mainContainer">
